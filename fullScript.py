d12682bcd3b7be3c96a7deacd9fcba44c3d8f189
Mahmoud Yazid <abuelyazid.software@gmail.com>
2024-09-30 10:19:49 +0100

    Löschen Sie unnötige Zeilen
    Löschen Sie unnötige Zeilen

--- fullScript.py
+++ fullScript.py
@@ -13,8 +13,7 @@ def ask_gpt(prompt):
         
         response = requests.post(url, headers=headers, json=data)
        
-        print(response.json())
-        print(response.json()["candidates"][0]["content"]["parts"][0]["text"])
+        
         return response.json()["candidates"][0]["content"]["parts"][0]["text"]
 
 def recognize_speech():
@@ -23,18 +22,18 @@ def recognize_speech():
 
             recognizer = sr.Recognizer()
             recognizer.adjust_for_ambient_noise(source) 
-            text = u"ich kann ihnen horen ?"  # Prefix the string with 'u' to make it Unicode
+            text = u"stell sie ein frage"  # Prefix the string with 'u' to make it Unicode
             text_encoded = text.encode('utf-8')  # Encode it to UTF-8
 
             # Now pass it to tts.say
             tts.say("{}".format(text_encoded))
-            print("start listening") # Adjust for ambient noise
+           
             audio = recognizer.listen(source)  # Listen for audio
 
             try:
                 # Recognize speech using Google Web Speech API
                 text = recognizer.recognize_google(audio, language='de-DE')
-                print("You said: " + text)
+               
                 return text
             except sr.UnknownValueError:
                 print("Sorry, I could not understand the audio.")
@@ -51,7 +50,7 @@ def connect_to_nao():
         text_encoded = response.encode('utf-8')
 
         tts.say("{}".format(text_encoded))
-        print(response )
+        
     except Exception as e:
         print("Could not connect to NAO: {}".format(e))
 
@@ -61,5 +60,16 @@ def CleanResponse (text):
 
 
 
-
-connect_to_nao()
+def AntwortNichtMehrAls3Liene():
+        url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyC7uO2DyDrB7HWUFXxLZ5ZxE7NNvtrbHIE"
+        headers = {
+            "Content-Type": "application/json"
+        }
+        data = {"contents": [{"parts": [{"text": "{}".format("Bitte antworten Sie kurz")}]}]}
+        
+        requests.post(url, headers=headers, json=data)
+       
+        
+while(True) :
+    AntwortNichtMehrAls3Liene()
+    connect_to_nao()
